f8a215112b85aaca07e79636bd7bec5ebff3abb9
Francis <franciskarani15@gmail.com>
2025-08-26 21:26:04 +0300

    creates a base module, creates anassociation table and a funding agency table

--- lib/models/associations.py
+++ lib/models/associations.py
@@ -1,16 +1,16 @@
 from sqlalchemy import Integer, Text, Column, ForeignKey
 from sqlalchemy.orm import relationship
-from lib.models.associations import Base
+from lib.models.base import Base
 
 
 # create an association table
 
 class Project_collaborations(Base):
     __tablename__ = "project_collaborations"
-    id = Column(Integer, primary_key=True)
+    # id = Column(Integer, primary_key=True)
     researchers_id = Column(Integer, ForeignKey("researchers.id"), primary_key = True)
     agency_id = Column(Integer, ForeignKey(
-        "agencies.id"), primary_key=True)
+        "funding_agencies.id"), primary_key=True)
     project_id = Column(Integer, ForeignKey(
         "projects.id"), primary_key=True)
 
--- lib/models/enitities.py
+++ lib/models/enitities.py
@@ -1,9 +1,10 @@
 from sqlalchemy import Integer, Column, String, VARCHAR, Text
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import relationship
 from lib.models.associations import Project_collaborations
+from lib.models.base import Base
 
 # cretes a base to inherit from
-Base = declarative_base()
+
 
 # creates the researchers class that inherits from base
 class Researcher(Base):
@@ -18,7 +19,24 @@ class Researcher(Base):
     projects = relationship(
         "Project",
         secondary= "project_collaborations",
+        back_populates="researchers"
     )
+
+# creates the funding agency class that will be used to create a table for the funding agencies in the db
+class Funding_agency(Base):
+    __tablename__ = "funding_agencies"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    country = Column(String, nullable=False)
+    contact_email= Column(String, nullable=False, unique=True)
+    # relationships an agency funds a project
+    projects = relationship(
+        "project",
+        secondary="project_collaborations",
+        back_populates="funding_agencies"
+    )
+
+
 
 
 
