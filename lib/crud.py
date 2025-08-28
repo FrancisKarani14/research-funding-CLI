dfefdcd020d16f7afa21971f20212988c866b77a
Francis <franciskarani15@gmail.com>
2025-08-28 09:55:18 +0300

    adds the crud.py file to add manager, projects, funders, researchers

--- lib/crud.py
+++ lib/crud.py
@@ -12,3 +12,24 @@ def add_project(researchers_name, title, description, start_date):
     session.add(project)
     session.commit()
 # add_project("Morris", "AI", "how to dhfsdkn  ksdfbsdc m mksbcmsd  ksdcbic", "12-3-2025")
+
+
+def add_researcher(first_name, last_name):
+    """Add a new researcher"""
+    researcher = Researcher(first_name=first_name, last_name=last_name)
+    session.add(researcher)
+    session.commit()
+
+
+def add_manager(first_name, last_name):
+    """Add a new manager"""
+    manager = Manager(first_name=first_name, last_name=last_name)
+    session.add(manager)
+    session.commit()
+
+
+def add_funder(name, budget):
+    """Add a new funding agency"""
+    funder = Funding_agency(name=name, budget=budget)
+    session.add(funder)
+    session.commit()
